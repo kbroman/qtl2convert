e50d057ae43cf6cebb8c90f3216ee1c25a774cf5
Karl Broman <kbroman@gmail.com>
2016-05-24 14:07:23 -0500

    Expand test of find_unique_geno

--- tests/testthat/test-find_unique_geno.R
+++ tests/testthat/test-find_unique_geno.R
@@ -1,17 +1,23 @@
 context("find_unique_geno")
 
 test_that("find_unique_geno works", {
-    g <- rbind(c("NA", "A",  "A",  "A", "T"),
-               c("NA", "NA", "NA", "A", "A"),
-               c("A",  "A",  "T",  "G", "G"),
-               c("C", "C",  "G",  "G", "NA"))
+    g <- rbind(c(NA,  "A",  "A",  "A", "T"),
+               c(NA,  NA,   NA,   "A", "A"),
+               c("A", "A",  "T",  "G", "G"),
+               rep(NA, 5),
+               c("N", "H",  "",   "A", "A"),
+               c("N", "H",  "",   "A", "T"),
+               c("C", "C",  "G",  "G", NA))
     expected <- rbind(c("A","T"),
                       c(NA, NA),
                       c(NA, NA),
+                      c(NA, NA),
+                      c(NA, NA),
+                      c("A", "T"),
                       c("C", "G"))
     expect_equal(find_unique_geno(g), expected)
 
-    rownames(g) <- rownames(expected) <- paste0("mar", 1:4)
+    rownames(g) <- rownames(expected) <- paste0("mar", 1:7)
     expect_equal(find_unique_geno(g), expected)
 
 })
