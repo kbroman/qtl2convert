2b533bdbe467b308db01cc9abe03a3abf1f140a5
Karl Broman <kbroman@gmail.com>
2017-09-13 09:30:44 -0500

    small fix to help file for find_consensus_geno
    - "See also" linked to itself; delete that and add link to encode_geno()

--- R/find_consensus_geno.R
+++ R/find_consensus_geno.R
@@ -10,7 +10,7 @@
 #'
 #' @return Vector of consensus genotypes, one value per row of \code{genotypes}
 #'
-#' @seealso \code{\link{find_consensus_geno}}, \code{\link{find_unique_geno}}
+#' @seealso \code{\link{find_unique_geno}}, \code{\link{encode_geno}}
 #' @export
 #' @examples
 #' g <- rbind(c("NA", "N", "A", "A", "T", "G", NA, "H"),
