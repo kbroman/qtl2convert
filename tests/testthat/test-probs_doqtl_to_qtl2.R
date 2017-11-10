36b0bcd25eb3f46565f26b62d939e56e42d9b9bb
Karl Broman <kbroman@gmail.com>
2017-11-10 16:34:23 -0600

    Add test of probs_doqtl_to_qtl2 and reverse

--- tests/testthat/test-probs_doqtl_to_qtl2.R
+++ tests/testthat/test-probs_doqtl_to_qtl2.R
@@ -30,3 +30,34 @@ test_that("probs_doqtl_to_qtl2 works", {
     expect_equal( result, expected )
 
 })
+
+
+test_that("probs convert between DOQTL and R/qtl2", {
+    if(isnt_karl()) skip("this test only run locally")
+
+    file <- paste0("https://raw.githubusercontent.com/rqtl/",
+                   "qtl2data/master/DOex/DOex.zip")
+    DOex <- read_cross2(file)
+
+    # subset to chr 2
+    DOex <- DOex[,"2"]
+
+    # calculate genotype probabilities and convert to allele probabilities
+    pr <- calc_genoprob(DOex, error_prob=0.002)
+
+    map_tab <- map_list_to_df(DOex$gmap, pos_column="cM")
+
+    # convert to DOQTL format
+    pr_doqtl <- probs_qtl2_to_doqtl(pr)
+
+    # convert back
+    pr_rqtl2 <- probs_doqtl_to_qtl2(pr_doqtl, map_tab)
+
+    expect_equal(pr_rqtl2, pr)
+
+    # convert back yet again
+    pr_doqtl_b <- probs_qtl2_to_doqtl(pr_rqtl2)
+
+    expect_equal(pr_doqtl_b, pr_doqtl)
+
+})
