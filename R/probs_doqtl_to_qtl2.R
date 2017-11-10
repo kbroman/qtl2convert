88ca42aba281ec42d620425588caca85f67848fd
Karl Broman <kbroman@gmail.com>
2017-11-10 16:03:34 -0600

    Fix probs_doqtl_to_qtl2() to deal properly with X chromosome

--- R/probs_doqtl_to_qtl2.R
+++ R/probs_doqtl_to_qtl2.R
@@ -8,6 +8,10 @@
 #'
 #' @param map Data frame with marker map
 #'
+#' @param is_female Optional logical vector indicating which
+#' individuals are female. Names should contain the individual
+#' identifiers, matching the row names in `probs`.
+#'
 #' @param chr_column Name of the column in `map` that contains the chromosome IDs.
 #'
 #' @param pos_column Name of the column in `map` that contains the marker positions.
@@ -17,9 +21,20 @@
 #'
 #' @return An object of the form produced by [qtl2geno::calc_genoprob()].
 #'
+#' @details
+#' We assume that the X chromosome is labeled `"X"` (must be
+#' upper-case) and that any other chromosomes are autosomes.
+#' We assume that the genotypes are labeled using the 8 letters A-H.
+#'
+#' If the probabilities are for the full 36 states and the X
+#' chromosome is included but `is_female` is not provided, we'll guess
+#' which individuals are females based on their genotype
+#' probabilities. (If the average, across loci, of the sum of the
+#' heterozygote probabilities is small, we'll assume it's a female.)
+#'
 #' @export
 probs_doqtl_to_qtl2 <-
-    function(probs, map, chr_column="chr", pos_column="cM", marker_column="marker")
+    function(probs, map, is_female=NULL, chr_column="chr", pos_column="cM", marker_column="marker")
 {
     if(is.null(marker_column)) {
         marker_column <- "qtl2tmp_marker"
@@ -40,6 +55,9 @@ probs_doqtl_to_qtl2 <-
 
     # subset probs to the portion in map
     in_map <- dimnames(probs)[[3]] %in% marker
+    if(sum(in_map)==0) {
+        stop("No markers in common between probs and map")
+    }
     if(any(!in_map)) {
         probs <- probs[,,in_map, drop=FALSE]
         warning("Omitting ", sum(!in_map), " markers from probs, not found in map")
@@ -48,6 +66,32 @@ probs_doqtl_to_qtl2 <-
     # make sure they're aligned
     m <- match(marker, dimnames(probs)[[3]])
     probs <- probs[,,m, drop=FALSE]
+
+    # number of genotypes
+    n_geno <- ncol(probs)
+    if(n_geno != 36 & n_geno != 8)
+        stop("number of genotypes (number of columns of probs) should be 8 or 36")
+
+    # align probabilities and is_female
+    if(!is.null(is_female) && n_geno==36) { # if n_geno==8, ignore is_female
+        ids1 <- names(is_female)
+        ids2 <- rownames(probs)
+        if(is.null(ids1) || is.null(ids2))
+            stop("probs and is_female must both contain individual identifiers")
+        both <- ids2[ids2 %in% ids1] # take order from probs
+        if(length(both) == 0)
+            stop("No individuals in common between probs and is_female")
+        n_sex_only <- sum(!(ids1 %in% both))
+        n_probs_only <- sum(!(ids2 %in% both))
+        if(n_sex_only > 0) {
+            warning("Omitting ", n_sex_only, " individuals absent from probs")
+        }
+        is_female <- is_female[both] # reorder no matter what
+        if(n_probs_only > 0) {
+            warning("Omitting ", n_probs_only, " individuals absent from is_female")
+            probs <- probs[both,,,drop=FALSE]
+        }
+    }
 
     # reorder from (AA, AB, AC, AD, AE, ...) to (AA, AB, BB, AC, BC, CC, AD, ...)
     mat <- rbind(rep(1:8, each=8), rep(1:8, 8))
@@ -59,28 +103,47 @@ probs_doqtl_to_qtl2 <-
 
     new_geno_order <- match(new_geno_labels, old_geno_labels)
 
-    n_geno <- ncol(probs)
-
     # split probs (and reorder if necessary)
     chr <- map[,chr_column]
-    if(is.factor(chr)) uchr <- levels(chr)
+    if(is.factor(chr)) {
+        chr <- factor(chr) # reduce to observed levels
+        uchr <- levels(chr)
+    }
     else uchr <- unique(chr)
 
     newprobs <- setNames(vector("list", length(uchr)), uchr)
     for(i in uchr) {
         if(n_geno == 36) {
-            newprobs[[i]] <- probs[,new_geno_order,chr==i]
+            newprobs[[i]] <- probs[,new_geno_order,chr==i,drop=FALSE]
         } else {
-            newprobs[[i]] <- probs[,, chr==i]
+            newprobs[[i]] <- probs[,, chr==i,drop=FALSE]
+        }
+
+        if(i == "X" && n_geno==36) { # need to pull apart the males and females
+            newprobs[[i]] <- array(0, dim=c(nrow(probs), ncol(probs)+8, sum(chr==i)))
+            dimnames(newprobs[[i]]) <- list(rownames(probs),
+                                            c(new_geno_order, paste0(LETTERS[1:8], "Y")),
+                                            dimnames(probs)[[3]])
+
+            if(is.null(is_female)) { # need to guess sex
+                het_tolerance <- 1e-4
+
+                hom <- paste0(LETTERS[1:8], LETTERS[1:8])
+                het <- old_geno_labels[!(old_geno_labels %in% hom)]
+                sum_het <- apply(probs[,het,chr==i], 1, sum)/dim(probs[[3]])
+                is_female <- setNames((sum_het > het_tolerance), rownames(probs))
+            }
+            if(any(is_female)) {
+                newprobs[[i]][is_female, 1:36, ] <- probs[is_female, new_geno_order, chr==i, drop=FALSE]
+            }
+            if(any(!is_female)) {
+                newprobs[[i]][!is_female, 37:44, ] <- probs[is_female, hom, chr==i, drop=FALSE]
+            }
         }
     }
 
     is_x_chr <- (uchr=="X")
     names(is_x_chr) <- uchr
-
-    # need to fix X chr probabilities, but we don't know individuals sex
-    # ... need to add it as an input
-
 
     if(n_geno == 8) {
         alleleprobs <- TRUE
