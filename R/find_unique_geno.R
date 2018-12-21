85d4f1ef7c805628adf22e4346658b33130012f1
Karl Broman <kbroman@gmail.com>
2018-12-21 15:39:03 -0600

    Fix bug re quiet global var in encode_geno etc
     - When revising count_unique_geno, encode_geno, find_consensus_geno,
       and find_unique_geno to use parallel calculations, there was a bug
       in setup_cluster() call.

--- R/find_unique_geno.R
+++ R/find_unique_geno.R
@@ -40,7 +40,7 @@ function(genotypes, na.strings=c("N", "H", "NA", ""), cores=1)
     # if no rows, return NULL
     if(nrow(genotypes)==0) return(NULL)
 
-    cores <- setup_cluster(cores, quiet)
+    cores <- setup_cluster(cores, quiet=TRUE)
 
     by_batch_func <- function(batch) .find_unique_geno(genotypes[batch,,drop=FALSE])
 
